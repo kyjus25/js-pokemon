6695327ecc84efe0647ede33a7f72cbf269fc4e5
Justin White <kyjus25@gmail.com>
2020-01-04 16:50:17 -0600

    Closest to animation so far

--- classes/keyboard.js
+++ classes/keyboard.js
@@ -2,4 +2,5 @@ window.addEventListener( "keypress", keyDown, false );
 
 function keyDown(e) {
   map.move(e.key);
+  player.playAnimation(e.key);
 }
--- classes/player.js
+++ classes/player.js
@@ -2,8 +2,35 @@ function Player() {
 
   let pos = [(width / 2) - (scale / 2), (height / 2) - (scale / 2)];
 
+  let animating = false;
+  let animation = [0, 0];
+
   this.setPosition = function (x, y) {
     pos = [x, y];
+  }
+
+  this.playAnimation = function (dir) {
+    switch(dir) {
+      case 'w':
+        animation = [animation[0] + 1, 3];
+        if (animation[0] > 3) {
+          animation[0] == 0;
+        }
+        break;
+      case 'a':
+        animation = [animation[0] + 1, 1];
+        break;
+      case 's':
+          animation = [animation[0] + 1, 0];
+        break;
+      case 'd':
+          animation = [animation[0] + 1, 2];
+        break;
+    }
+    if (animation[0] > 3) {
+      animation[0] = 0;
+    }
+    console.log('animation', animation);
   }
 
   this.update = function (progress) {
@@ -12,9 +39,11 @@ function Player() {
 
   this.render = function (ctx) {
     const player = document.getElementById("player");
+    const spriteDimension = 256 / 4;
     const x = 0; // (256 / 4)
     const y = 0;
-    ctx.drawImage(player, x, y, 256 / 4, 256 / 4, pos[0], pos[1], scale, scale);
+    // his.playAnimation('a');
+    ctx.drawImage(player, animation[0] * spriteDimension, animation[1] * spriteDimension, spriteDimension, spriteDimension, pos[0], pos[1], scale, scale);
   }
 
 }
